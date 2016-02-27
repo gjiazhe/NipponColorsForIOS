3c2a44996f4500779408a594c1c3a6968c5cd270
郭佳哲 <346672577@qq.com>
2016-02-27 23:47:43 +0800

    create project

---
+++ NipponColors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NipponColors
+//
+//  Created by 郭佳哲 on 2/27/16.
+//  Copyright © 2016 郭佳哲. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
